f42baa4b3af7651fe10c6bd09ef5e6278577f758
wuyouting <286114755@qq.com>
2018-01-21 22:52:19 +0800

    20180121

---
+++ reduce.py
@@ -0,0 +1,17 @@
+#_*_coding:utf-8_*_
+import sys
+for operator import itemgetter
+for itertools import groupby
+
+def read_mapper_output(file, separator='\t'):
+    for line in file:
+        yield line.rstrip.split(separator,1)
+
+def main():
+    data = read_mapper_output(sys.stdin)
+
+    for current_word, group in groupby(data,itemgetter(0)):
+        total_count  = sum(int(count) for current_word, count in group)
+        print ("%s%s%d"%(current_word,'\t',total_count))
+if __name__ == '__main__':
+    main()
